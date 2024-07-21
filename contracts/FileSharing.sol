4015cea999a263de80b0204d0951e98d907f9536
BRKon123 <114494803+BRKon123@users.noreply.github.com>
2024-07-21 04:30:06 +0100

    update filesharing.sol so that each member in access list has associated encrypted key they can use to decrypt the ipfs data

--- contracts/FileSharing.sol
+++ contracts/FileSharing.sol
@@ -5,17 +5,17 @@ contract FileSharing {
     struct File {
         string cid;
         address owner;
-        mapping(address => bool) accessList;
+        mapping(address => string) accessList; // Mapping from address to encrypted key
     }
 
     mapping(uint256 => File) public files;
     uint256 public fileCount;
 
     event FileAdded(uint256 fileId, string cid, address owner);
-    event AccessGranted(uint256 fileId, address grantee);
+    event AccessGranted(uint256 fileId, address grantee, string encryptedKey);
     event AccessRevoked(uint256 fileId, address revokee);
 
-    function grantAccess(uint256 fileId, address grantee, string memory cid) public {
+    function grantAccess(uint256 fileId, address grantee, string memory cid, string memory encryptedKey) public {
         File storage file = files[fileId];
 
         // If the file does not exist, add it
@@ -28,20 +28,27 @@ contract FileSharing {
             require(msg.sender == file.owner, "Only the owner can grant access");
         }
 
-        file.accessList[grantee] = true;
-        emit AccessGranted(fileId, grantee);
+        file.accessList[grantee] = encryptedKey;
+        emit AccessGranted(fileId, grantee, encryptedKey);
     }
 
     function revokeAccess(uint256 fileId, address revokee) public {
         File storage file = files[fileId];
         require(file.owner != address(0), "File does not exist");
         require(msg.sender == file.owner, "Only the owner can revoke access");
-        file.accessList[revokee] = false;
+        delete file.accessList[revokee];
         emit AccessRevoked(fileId, revokee);
     }
 
     function hasAccess(uint256 fileId, address user) public view returns (bool) {
         File storage file = files[fileId];
-        return file.owner != address(0) && file.accessList[user];
+        return file.owner != address(0) && bytes(file.accessList[user]).length > 0;
+    }
+
+    function getEncryptedKey(uint256 fileId, address user) public view returns (string memory) {
+        File storage file = files[fileId];
+        require(file.owner != address(0), "File does not exist");
+        require(bytes(file.accessList[user]).length > 0, "No access for this user");
+        return file.accessList[user];
     }
 }
